f68231ca882ba96fa8b7afdfe3d02893f738c022
Sanchit Kumar <sanchit.kumar@ibm.com>
2021-10-22 00:52:52 -0400

    react 3rd commit

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <h1>React first app</h1>
+        <h2>header</h2>
         <a
           className="App-link"
           href="https://reactjs.org"
